d2182be75eb5b6120d3bfaa7710afd2b0a6d6513
App Academy Student <student@appacademy.io>
2013-09-24 11:01:38 -0700

    Added board and piece structure

--- chess.rb
+++ chess.rb
@@ -1,2 +1,89 @@
 class Board
+  def initialize
+    @board = Array.new(8) { Array.new(8) }
+  end
+end
+
+module SlidingPieces
+
+  def moves
+
+  end
+end
+
+class Rook
+
+  include SlidingPieces
+
+  def initialize
+
+  end
+
+  def move_dirs
+
+  end
+end
+
+class Queen
+  include SlidingPieces
+  def initialize
+
+  end
+
+  def move_dirs
+
+  end
+end
+
+class Bishop
+  include SlidingPieces
+  def initialize
+
+  end
+
+  def move_dirs
+
+  end
+end
+
+module SteppingPieces
+
+  def moves
+
+  end
+end
+
+class King
+  include SteppingPieces
+  def initialize
+
+  end
+
+  def move_dirs
+
+  end
+end
+
+class Knight
+  include SteppingPieces
+  def initialize
+
+  end
+
+  def move_dirs
+
+  end
+
+end
+
+class Pawn
+
+  def initialize
+
+  end
+
+  def moves
+
+  end
+
 end
